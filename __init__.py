d5347a5135dc3639e791f7a7d65f371848c1813d
benarc <ben@arc.wales>
2024-06-05 12:23:28 +0100

    Updated listener task to most recent

--- __init__.py
+++ __init__.py
@@ -4,7 +4,7 @@ from fastapi import APIRouter
 
 from lnbits.db import Database
 from lnbits.helpers import template_renderer
-from lnbits.tasks import catch_everything_and_restart
+from lnbits.tasks import create_permanent_unique_task
 
 
 db = Database("ext_myextension")
@@ -30,7 +30,15 @@ from .tasks import wait_for_paid_invoices
 from .views import *
 from .views_api import *
 
+scheduled_tasks: list[asyncio.Task] = []
+
+def myextension_stop():
+    for task in scheduled_tasks:
+        try:
+            task.cancel()
+        except Exception as ex:
+            logger.warning(ex)
 
 def myextension_start():
-    loop = asyncio.get_event_loop()
-    loop.create_task(catch_everything_and_restart(wait_for_paid_invoices))
+    task = create_permanent_unique_task("ext_myextension", wait_for_paid_invoices)
+    scheduled_tasks.append(task)
